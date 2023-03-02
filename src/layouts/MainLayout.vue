9241cb6eee6e3105724d3eebb3c821c3a4037992
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2023-03-02 18:01:17 +0700

    Commit Project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,112 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> First Project CRUD </q-toolbar-title>
+
+        <div>Legal Analytics</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Binusmaya",
+    caption: "binusmaya.binus.ac.id",
+    icon: "school",
+    link: "https://newbinusmaya.binus.ac.id",
+  },
+  {
+    title: "Annas's Github",
+    caption: "github.com/AnnasIsmail",
+    icon: "code",
+    link: "https://github.com/AnnasIsmail",
+  },
+  {
+    title: "Farhan's Github ",
+    caption: "github.com/fluorineheit",
+    icon: "code",
+    link: "https://github.com/fluorineheit",
+  },
+  {
+    title: "Laras' Github",
+    caption: "github.com/lareinalaras",
+    icon: "code",
+    link: "https://github.com/lareinalaras",
+  },
+  {
+    title: "Farhan's Twitter",
+    caption: "@Fluorine_heit",
+    icon: "public",
+    link: "https://twitter.com/Fluorine_heit",
+  },
+  {
+    title: "Laras' Spotify",
+    caption: "open.spotify.com/Lareina.",
+    icon: "favorite",
+    link: "https://open.spotify.com/user/5aqfmockw7lqjstjxps5x3ruj?si=e80ed56121184955",
+  },
+  {
+    title: "Spotify orang ganteng",
+    caption: "Klik saya",
+    icon: "favorite",
+    link: "https://open.spotify.com/user/21w5qjfckoxkiy4kaf53tqp5i?si=zfbgue8mSjyQecl9Cih8UQ",
+  },
+  {
+    title: "Annas's Youtube",
+    caption: "Click me",
+    icon: "favorite",
+    link: "https://www.youtube.com/channel/UCWBDexy4KU6WrHwR74B-Nfg",
+  },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,157 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="q-pa-md" style="width: 85vw">
+      <q-form @submit.prevent="addToDo" @reset="onReset" class="q-gutter-md">
+        <q-input
+          v-model="name"
+          label="What do you want to do? *"
+          hint="example: lazy-lazyan"
+        />
+
+        <div>
+          <q-btn label="Submit" type="submit" color="primary" />
+          <q-btn label="Reset" type="reset" color="primary" class="q-ml-sm" />
+        </div>
+      </q-form>
+
+      <div class="q-pa-md">
+        <q-list bordered separator>
+          <q-item v-for="(item, index) in Todo" :key="item">
+            <q-item-section>
+              <q-item-label>{{ item.todo }}</q-item-label>
+              <q-item-label v-if="!item.edit" caption>{{
+                item.dateTime
+              }}</q-item-label>
+              <q-input v-else v-model="valueEdit" />
+              <q-item-label v-if="item.edited" caption
+                >edited: {{ item.edited }}</q-item-label
+              >
+            </q-item-section>
+
+            <div class="text-center">
+              <!-- <q-icon clickable name="edit" color="teal" size="25px" /> -->
+              <q-btn
+                @click="editItem(index, item.todo)"
+                color="primary"
+                icon="edit"
+                v-if="!item.edit"
+                style="padding-left: 5px; padding-right: 5px"
+              />
+              <q-btn
+                @click="deleteItem(index)"
+                color="negative"
+                icon="delete"
+                v-if="!item.edit"
+                style="margin-left: 10px; padding-left: 5px; padding-right: 5px"
+              />
+              <q-btn
+                @click="saveEdit(index)"
+                color="primary"
+                icon="check"
+                v-if="item.edit"
+                style="margin-left: 10px; padding-left: 5px; padding-right: 5px"
+              />
+            </div>
+          </q-item>
+        </q-list>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { useQuasar } from "quasar";
+import { ref } from "vue";
+
+export default {
+  data() {
+    return {
+      Todo: JSON.parse(localStorage.getItem("Todo")),
+      valueEdit: "",
+    };
+  },
+
+  mounted() {
+    if (JSON.parse(localStorage.getItem("Todo")) === null) {
+      localStorage.setItem("Todo", JSON.stringify([]));
+    }
+  },
+
+  setup() {
+    const $q = useQuasar();
+    const name = ref(null);
+
+    return {
+      name,
+
+      onSubmit() {
+        Todo.push(name.value);
+
+        if (accept.value !== true) {
+          $q.notify({
+            color: "red-5",
+            textColor: "white",
+            icon: "warning",
+            message: "You need to accept the license and terms first",
+          });
+        } else {
+          $q.notify({
+            color: "green-4",
+            textColor: "white",
+            icon: "cloud_done",
+            message: "Submitted",
+          });
+        }
+      },
+
+      onReset() {
+        name.value = null;
+      },
+    };
+  },
+
+  methods: {
+    addToDo() {
+      const today = new Date();
+      const date = today.toLocaleDateString();
+      const time = today.toLocaleTimeString();
+
+      if (this.name !== "" && this.name !== null) {
+        this.Todo.push({
+          todo: this.name,
+          dateTime: `${date} ${time}`,
+          edit: false,
+          edited: false,
+        });
+        localStorage.setItem("Todo", JSON.stringify(this.Todo));
+        this.name = null;
+      }
+    },
+    editItem(param, text) {
+      this.valueEdit = text;
+      this.Todo[param].edit = true;
+    },
+    saveEdit(param) {
+      const today = new Date();
+      const date = today.toLocaleDateString();
+      const time = today.toLocaleTimeString();
+
+      this.Todo[param].todo = this.valueEdit;
+      this.Todo[param].edit = false;
+      this.Todo[param].edited = `${date} ${time}`;
+      localStorage.setItem("Todo", JSON.stringify(this.Todo));
+    },
+    deleteItem(param) {
+      this.Todo.splice(param, 1);
+      localStorage.setItem("Todo", JSON.stringify(this.Todo));
+    },
+  },
+
+  // computed: {
+  //   mappedData() {
+  //     return this.arrayData.map((item) => item);
+  //   },
+  // },
+};
+</script>
